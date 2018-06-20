a811a9192a9eca182456124aca3a914d597e2e9c
Hannah Higgins <meowhannah@hotmail.co.uk>
2018-06-20 21:59:50 +0100

    Added Game Scene
    Added the Game Play Scene along with a game timer

---
+++ Assets/OpenMainSceneScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenMainSceneScript : MonoBehaviour {
+
+	public void GoToNextSceneAction () {
+		Application.LoadLevel ("GamePlay Scene");
+	}
+}
