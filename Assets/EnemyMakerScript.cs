f507c3d4bbe26d91de6a7a6e4e5c8462c09ce1d8
Hannah Higgins <meowhannah@hotmail.co.uk>
2018-06-25 16:27:47 +0100

    Added Enemy Generation
    Added Enemy Generation and Physics

---
+++ Assets/EnemyMakerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMakerScript : MonoBehaviour {
+
+	public GameObject enemyGo;
+	public Sprite[] myImages;
+
+	void Start () {
+		InvokeRepeating ("makeAnEnemyAction", 0, 3);
+	}
+
+	void makeAnEnemyAction() {
+		GameObject newEnemyGo = (GameObject) Instantiate (enemyGo) as GameObject;
+		float x = Random.Range (-11.0f, 11.0f);
+		float y = -6;
+		float z = 4;
+		newEnemyGo.transform.position = new Vector3 (x, y, z);
+		newEnemyGo.GetComponent<SpriteRenderer> ().sprite = myImages [Random.Range(0, myImages.Length)];
+		newEnemyGo.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * 600);
+	}
+}
