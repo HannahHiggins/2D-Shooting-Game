b613ccca92a33287120d919f6d29452a13d409c2
Hannah Higgins <meowhannah@hotmail.co.uk>
2018-06-26 14:19:03 +0100

    Added Game Over
    Added Game Over menu and mechanisms

--- Assets/gamePlayScript.cs
+++ Assets/gamePlayScript.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class gamePlayScript : MonoBehaviour {
+	public GameObject gameOverPanel;
 	AudioSource gunShotAudioSource;
 	public AudioClip gunShotClip;
+	bool gameIsPaused = false;
 
 	public GameObject timerTextView;
 	public GameObject scoreTextView;
@@ -26,14 +28,18 @@ public class gamePlayScript : MonoBehaviour {
 	}
 	
 	void timeCounterAction(){
-		if (gameTime > 0) {
+		if (gameTime >= 0) {
 			timeMinutes = (int)(gameTime / 60);
 			timeSeconds = gameTime % 60;
 			string timerTextString = timeMinutes.ToString ("D1") + "\' " + timeSeconds.ToString ("D2") + "\"";
 			timerTextView.GetComponent<Text> ().text = timerTextString;
 			gameTime = gameTime - 1;
 		} else {
-			Debug.Log ("Game is Over");
+			Time.timeScale = 0;
+			gameIsPaused = true;
+			gameOverPanel.GetComponent<CanvasGroup> ().alpha = 1;
+			gameOverPanel.GetComponent<CanvasGroup> ().interactable = true;
+			gameOverPanel.GetComponent<CanvasGroup> ().blocksRaycasts = true;
 		}
 	}
 
@@ -53,30 +59,46 @@ public class gamePlayScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp (KeyCode.Space)) {
-			shootAction ();
+		if (!gameIsPaused) {
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				shootAction ();
+			}
+
+			bazookaGo.transform.LookAt (crossHairGo.transform);
+
+			float h = Input.GetAxis ("Mouse X") / 5;
+			float v = Input.GetAxis ("Mouse Y") / 5;
+
+			Vector3 crossHairMov = new Vector3 (h, v, 0);
+			crossHairGo.transform.Translate (crossHairMov);
+
+			float x = crossHairGo.transform.position.x;
+			float y = crossHairGo.transform.position.y;
+
+			if (x > 10)
+				x = 10;
+			if (x < -10)
+				x = -10;
+			if (y > 7)
+				y = 7;
+			if (y < -5)
+				y = -5;
+
+			crossHairGo.transform.position = new Vector3 (x, y, 0);
 		}
+	}
 
-		bazookaGo.transform.LookAt (crossHairGo.transform);
+	public void RestartAction(){
+		Time.timeScale = 1;
+		gameIsPaused = false;
+		gameOverPanel.GetComponent<CanvasGroup> ().alpha = 0;
+		gameOverPanel.GetComponent<CanvasGroup> ().interactable = false;
+		gameOverPanel.GetComponent<CanvasGroup> ().blocksRaycasts = false;
 
-		float h = Input.GetAxis ("Mouse X") / 5;
-		float v = Input.GetAxis ("Mouse Y") / 5;
-
-		Vector3 crossHairMov = new Vector3(h, v, 0);
-		crossHairGo.transform.Translate (crossHairMov);
-
-		float x = crossHairGo.transform.position.x;
-		float y = crossHairGo.transform.position.y;
-
-		if (x > 10)
-			x = 10;
-		if (x < -10)
-			x = -10;
-		if (y > 7)
-			y = 7;
-		if (y < -5)
-			y = -5;
-
-		crossHairGo.transform.position = new Vector3 (x, y, 0);
+		gameTime = 150;
+		enemyKilled = 0;
+		roundsShot = 0;
+		scoreTextView.GetComponent<Text> ().text = enemyKilled.ToString () + " / " + roundsShot.ToString ();
+		timeCounterAction ();
 	}
 }
